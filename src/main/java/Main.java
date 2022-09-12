e9528898339c6919e9d3b66a2ba61d732458b91d
Amir Tafkhimi <amir.tafkhimi@gmail.com>
2022-09-12 12:54:49 +0200

    Add step 1

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import Model.Product;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // ProductListe
+        Product product1 = new Product(1, "Shoe");
+        Product product2 = new Product(2, "Camera");
+        Product product3 = new Product(3, "Laptop");
+
+
+
+    }
+}
